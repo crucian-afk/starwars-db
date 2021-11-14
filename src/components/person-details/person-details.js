ae9f4036417e29794a83515cd17ef208577c34f5
Юрий <y.headman@ya.ru>
2021-11-14 23:22:36 +0300

    Добавляет подробный вид персонажей и их список

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -57,29 +57,6 @@ export default class PersonDetails extends Component {
         }
 
         return (
-            // <div className="person-details card">
-            //     <img className="person-image"
-            //          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-            //          alt={`${name}`} />
-            //
-            //     <div className="card-body">
-            //         <h4>{name}</h4>
-            //         <ul className="list-group list-group-flush">
-            //             <li className="list-group-item">
-            //                 <span className="term">Gender</span>
-            //                 <span>{gender}</span>
-            //             </li>
-            //             <li className="list-group-item">
-            //                 <span className="term">Birth Year</span>
-            //                 <span>{birthYear}</span>
-            //             </li>
-            //             <li className="list-group-item">
-            //                 <span className="term">Eye Color</span>
-            //                 <span>{eyeColor}</span>
-            //             </li>
-            //         </ul>
-            //     </div>
-            // </div>
             <div className="person-details card">
                 {spinner}
                 {content}

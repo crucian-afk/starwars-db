f8edcee355a878a2f2e692ef86703de837a4ba7f
Юрий <y.headman@ya.ru>
2021-11-15 00:54:58 +0300

    Немного рефакторит код

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,8 +4,14 @@ import './app.css';
 import Header from "../header";
 import RandomPlanet from "../random-planet";
 import PeoplePage from "../people-page";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import SwapiService from "../../services/swapi-service";
+import ErrorIndicator from "../error-indicator";
 
 export default class App extends Component {
+
+    swapiService = new SwapiService();
 
     state = {
         selectedPerson: 5
@@ -15,6 +21,10 @@ export default class App extends Component {
         this.setState({
             selectedPerson: id
         })
+    }
+
+    componentDidCatch() {
+        return <ErrorIndicator />
     }
 
     render() {
